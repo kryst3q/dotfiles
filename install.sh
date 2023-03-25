f0bdae32cf3e2e80cfc12974f23ab350ee6a8793
Krystian Kulesz <krystian.kulesz@gmail.com>
2023-03-26 00:40:05 +0100

    systemd | add starting i3-wm by systemd together with other graphic-related daemons

--- install.sh
+++ install.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-########
-# nvim #
-########
-mkdir -p "${HOME}/.config/nvim/undo"
-ln -sf "${HOME}/dotfiles/nvim/init.vim" "${HOME}/.config/nvim"
+# TODO: check if zsh is set as SHELL
 
+if [ $(uname) = "Linux" ]; do
+    # # Make enabling default keyring possible at login
+    # # TODO: check if gnome-keyring was installed
+    # echo "auth       optional     pam_gnome_keyring.so" >> /etc/pam.d/login
+    # echo "session    optional     pam_gnome_keyring.so auto_start" >> /etc/pam.d/login
+
+    # TODO: check if xorg and xorg-xinit are installed
+    cp ./.xinitrc ${HOME}/
+done
+
+cp ./zsh/.config/zsh/.zshenv ${HOME}/
+
+# TODO: check if GNU Stow is installed
+
+for i in * ; do
+  if [ -d "$i" ]; then
+    stow ${i} 
+  fi
+done
+
+if [ $(uname) = "Linux" ]; do
+    for i in ${XDG_CONFIG_HOME}/systemd/user/*.service ; do
+      if [ -f "$i" ]; then
+        systemctl --user enable ${i}
+      fi
+    done
+done
