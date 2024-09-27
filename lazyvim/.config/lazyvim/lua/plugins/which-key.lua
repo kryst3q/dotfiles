4985a8e3043091beeea98659ec0373aebd8f36d4
Krystian Kulesz <krystian.kulesz@gmail.com>
2024-09-27 10:15:06 +0200

    lazyvim | update dependencies

---
+++ lazyvim/.config/lazyvim/lua/plugins/which-key.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>h"] = { name = "+http" },
+        ["<leader>t"] = { name = "+test" },
+      },
+    },
+  },
+}
